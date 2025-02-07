ce71a59b78838fd64d88855a4b4fb9a3e649a75f
ShantanuGarg2004 <shantanugarg2004@gmail.com>
2025-02-07 22:06:39 +0530

    Squashed 'frontend/' changes from e973bf5..f7a1a78
    f7a1a78 service page
    48e16b0 making perfect
    bcaa356 Merge branch 'main' of https://github.com/Ravi5691/newKrip
    1a41724 faq add and hero component
    
    git-subtree-dir: frontend
    git-subtree-split: f7a1a78d0d4c269f321ebb2a23abf4b3d2e05b29

--- src/components/stat_card.jsx
+++ src/components/stat_card.jsx
@@ -1,30 +1,48 @@
-import React from 'react'
+import React from "react";
 
 const StatCard = () => {
   return (
-    <div class="flex justify-around items-center w-screen h-screen">
-        <div className=' flex w-[80%]'>
+    <div class="flex items-center w-screen">
+      <div className=" flex w-full px-40">
         <div class="text-left w-4/6">
-            <h1 className='text-3xl'>Numbers are <br/> Telling our Story</h1>
-            <span class="leading-12">Showing the stats of Product</span>
+          <h1 className="text-4xl font-semibold pb-3">
+            Numbers are <br /> Telling our Story
+          </h1>
+          <span class="leading-12 text-">Showing the stats of Product</span>
         </div>
-        <div class="flex flex-col bg-gray-300 border-2 rounded-xl w-2/6">
-            <div class=" px-10 py-5 border-b-2">
-                <h1>300 k</h1>
-                <span>Views on the Video</span>
-            </div>
-            <div class=" px-10 py-5 border-b-2">
-                <h1>300 k</h1>
-                <span>Views on the Video</span>
-            </div>
-            <div class=" px-10 py-5">
-                <h1>300 k</h1>
-                <span>Views on the Video</span>
-            </div>
+        <div class="flex flex-col border-2 gap-y-0.5 border-[#060E0E] rounded-xl w-2/6 relative">
+          <div class=" px-10 py-7 bg-[#C8FFD41A] rounded-t-2xl">
+            <h1 className="text-3xl font-bold py-1">300 K</h1>
+            <span className="text-sm">Views on the Video</span>
+          </div>
+          <div class=" px-10 py-7 bg-[#C8FFD41A]">
+            <h1 className="text-3xl font-bold py-1">300 K</h1>
+            <span className="text-sm">Views on the Video</span>
+          </div>
+          <div class=" px-10 py-7 bg-[#C8FFD41A] rounded-b-2xl">
+            <h1 className="text-3xl font-bold py-1">300 K</h1>
+            <span className="text-sm">Views on the Video</span>
+          </div>
+          <div
+            className="absolute w-100 h-20 bg-[#83ff98a2] opacity-50 bg-blend-saturation top-40 rounded-full pointer-events-none"
+            style={{
+              transform: "rotate(140deg)",
+              filter: "blur(75px) ",
+              zIndex: 50,
+            }}
+          />
+          <div
+            className="absolute w-30 h-5 bg-[#83ff98a2] opacity-90 bg-blend-saturation top-45 left-40 rounded-full pointer-events-none"
+            style={{
+              transform: "rotate(140deg)",
+              filter: "blur(75px) ",
+              zIndex: 50,
+            }}
+          />
         </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default StatCard
+export default StatCard;
