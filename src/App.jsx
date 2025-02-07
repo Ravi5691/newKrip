f7a1a78d0d4c269f321ebb2a23abf4b3d2e05b29
Ravinder Singh Bisht <ravinderbisht5691@gmail.com>
2025-02-07 17:33:16 +0530

    service page

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 // import { HeroScroll } from './components/HeroScroll'
 // import { AppleCardsCarousel } from './components/AppleCardCarousel'
 import Header from './components/header'
+import Home from './components/servicePage/selectService'
 
 function App() {
 
   return (
    <div>
-    <Header/>
-    {/* <HeroScroll/> */}
-    {/* <AppleCardsCarousel/> */}
+    {/* <Header/> */}
+    <Home/>
    </div>
   )
 }
