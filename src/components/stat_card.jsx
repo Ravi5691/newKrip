1263c51eacd7c0c30a78a76c5593c574dc9fa831
Ravinder Singh Bisht <ravinderbisht5691@gmail.com>
2025-02-15 19:50:24 +0530

    frontpage responsive

--- src/components/stat_card.jsx
+++ src/components/stat_card.jsx
@@ -3,25 +3,25 @@ import React from "react";
 const StatCard = () => {
   return (
     <div class="flex items-center w-screen">
-      <div className=" flex w-full px-40">
-        <div class="text-left w-4/6">
-          <h1 className="text-4xl font-semibold pb-3">
+      <div className=" flex w-full lg:px-40 px-5">
+        <div class="text-left lg:w-4/6 pr-8 lg:pr-0">
+          <h1 className="lg:text-4xl text-lg font-semibold lg:pb-3">
             Numbers are <br /> Telling our Story
           </h1>
-          <span class="leading-12 text-">Showing the stats of Product</span>
+          <span class="leading-12 lg:text-base text-[10px]">Showing the stats of Product</span>
         </div>
-        <div class="flex flex-col border-2 gap-y-0.5 border-[#060E0E] rounded-xl w-2/6 relative">
-          <div class=" px-10 py-7 bg-[#C8FFD41A] rounded-t-2xl">
-            <h1 className="text-3xl font-bold py-1">300 K</h1>
-            <span className="text-sm">Views on the Video</span>
+        <div class="flex flex-col border-2 gap-y-0.5 border-[#060E0E] rounded-xl lg:w-2/6 w-1/2 relative">
+          <div class=" lg:px-10 lg:py-7 px-4 py-3 bg-[#C8FFD41A] rounded-t-2xl">
+            <h1 className="lg:text-3xl text-lg font-bold py-1">300 K</h1>
+            <span className="lg:text-sm text-[10px]">Views on the Video</span>
           </div>
-          <div class=" px-10 py-7 bg-[#C8FFD41A]">
-            <h1 className="text-3xl font-bold py-1">300 K</h1>
-            <span className="text-sm">Views on the Video</span>
+          <div class=" lg:px-10 lg:py-7 px-4 py-3 bg-[#C8FFD41A]">
+            <h1 className="lg:text-3xl text-lg font-bold py-1">300 K</h1>
+            <span className="lg:text-sm text-[10px]">Views on the Video</span>
           </div>
-          <div class=" px-10 py-7 bg-[#C8FFD41A] rounded-b-2xl">
-            <h1 className="text-3xl font-bold py-1">300 K</h1>
-            <span className="text-sm">Views on the Video</span>
+          <div class=" lg:px-10 lg:py-7 px-4 py-3 bg-[#C8FFD41A] rounded-b-2xl">
+            <h1 className="lg:text-3xl text-lg font-bold py-1">300 K</h1>
+            <span className="lg:text-sm text-[10px]">Views on the Video</span>
           </div>
           <div
             className="absolute w-100 h-20 bg-[#83ff98a2] opacity-50 bg-blend-saturation top-40 rounded-full pointer-events-none"
