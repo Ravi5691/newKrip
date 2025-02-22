8f4a0e24a0fc7710cb225bf830e285c604b77540
Ravinder Singh Bisht <ravinderbisht5691@gmail.com>
2025-02-22 12:38:18 +0530

    about us page and typewriting efect

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,16 @@ import SpecSheet from "./components/servicePage/SpecSheet";
 import AboutProject from "./components/servicePage/webDevService/addDiscription";
 import AddTags from "./components/servicePage/webDevService/chooseTag";
 import WebTemplate from "./components/servicePage/webDevService/webTemplate"; // Adjust the import based on your file structure
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
 import { SignupFormDemo } from "./components/signupForm";
+import AboutUs from "./components/aboutUs";
+import ContactUs from "./components/contactUs";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Header />} />
+        <Route path="/" element={<Header/>} />
         <Route path="/selectservice" element={<SelectService />} />
         <Route path="/web-template" element={<WebTemplate />} />
         <Route path="/adddescription" element={<AboutProject />} />
@@ -20,6 +22,8 @@ function App() {
         <Route path="/confirmationPage" element={<ConfirmationPage />} />
         <Route path="/specsheet" element={<SpecSheet />} />
         <Route path="/signup" element={<SignupFormDemo/>}/>
+        <Route path="/aboutus" element={<AboutUs/>}/>
+        <Route path="/contactus" element={<ContactUs/>}/>
       </Routes>
     </BrowserRouter>
   );
